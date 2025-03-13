1d53800b733f77a1a7ede30704dc0e6dd788e5c7
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-03-12 22:46:16 -0400

    feat: rotational function

--- Main.java
+++ Main.java
@@ -9,41 +9,15 @@ class Main {
 
         // EXAMPLE CODE FOR PRINTJAVA
         // TODO: Implement Meshes to have rotation and scaling vectors
-        // TODO: Possible future geometry: torus, lathe (line revolved around axis), text
+        // TODO: Possible future geometry: torus, lathe (line revolved around axis), text, 3d graph
 
-		// create the stl file
-        // param is the name of the file
 		STL f = new STL("test");
 
-        // create and add a cube
-        Rect cube = new Rect(1);
-        // set the position in space to a new point in space
-        cube.position = new Point(1.2, 1, 1.2);
-		f.add(cube);
+        Graph g = new Graph();
+		f.add(g);
 
-        Sphere sphere = new Sphere(0.5);
-        sphere.scale = new Point(3,1,1);
-        sphere.position = new Point(-1.2, 0, -1.2);
-        f.add(sphere);
-
-        Pyramid pyramid = new Pyramid(1);
-        f.add(pyramid);
-
-        // commented out because it makes it hard to see the other shapes
-        // just a 2d plane which is 10x10
-        //Plane plane = new Plane(10);
-        //f.add(plane);
-
-        Cylinder cylinder = new Cylinder(0.5, 1);
-        cylinder.position = new Point(-1.2, 0, 1.2);
-        f.add(cylinder);
-
-        Cone cone = new Cone(0.5, 1);
-        cone.position = new Point(1.2, 0, -1.2);
-        f.add(cone);
-
-		// write the file to the disk
 		f.write();
+
 		// display the file in a window (soon?)
 		//f.show();
     }
--- printjava/printjava/STL.java
+++ printjava/printjava/STL.java
@@ -20,42 +20,74 @@ public class STL {
 		this.meshes.add(m);
 	}
 
+	private Point rotatePoint(Point p, double rx, double ry, double rz, double theta) {
+		double cosTheta = Math.cos(theta);
+		double sinTheta = Math.sin(theta);
+	
+		double crossX = ry * p.z - rz * p.y;
+		double crossY = rz * p.x - rx * p.z;
+		double crossZ = rx * p.y - ry * p.x;
+	
+		double dot = rx * p.x + ry * p.y + rz * p.z;
+	
+		double newX = p.x * cosTheta + crossX * sinTheta + rx * dot * (1 - cosTheta);
+		double newY = p.y * cosTheta + crossY * sinTheta + ry * dot * (1 - cosTheta);
+		double newZ = p.z * cosTheta + crossZ * sinTheta + rz * dot * (1 - cosTheta);
+	
+		return new Point(newX, newY, newZ);
+	}
+
 	/*
 	 * logic for writing stl file to disk
 	 */
 	public void write() throws IOException {
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.name + ".stl"))) {
-			for(int i = 0; i < this.meshes.size(); i++) {
+			for (int i = 0; i < this.meshes.size(); i++) {
 				writer.write(String.format("solid %s\n", i));
 				Mesh m = this.meshes.get(i);
-
+	
 				double mx = m.position.x - m.anchor.x;
 				double my = m.position.y - m.anchor.y;
 				double mz = m.position.z - m.anchor.z;
-
+	
 				double rx = m.rotation.x;
 				double ry = m.rotation.y;
 				double rz = m.rotation.z;
-
+				double rotationAngle = Math.sqrt(rx * rx + ry * ry + rz * rz);
+	
+				if (rotationAngle != 0) {
+					rx /= rotationAngle;
+					ry /= rotationAngle;
+					rz /= rotationAngle;
+				}
+	
 				double sx = m.scale.x;
 				double sy = m.scale.y;
 				double sz = m.scale.z;
-
-				for (Triangle t : this.meshes.get(i).triangles) {
-					double tx = t.normal.x + mx;
-					double ty = t.normal.y + my;
-					double tz = t.normal.z + mz;
+	
+				for (Triangle t : m.triangles) {
+					Point rotatedNormal = (rotationAngle != 0) ? rotatePoint(t.normal, rx, ry, rz, rotationAngle) : t.normal;
+	
+					double tx = rotatedNormal.x + mx;
+					double ty = rotatedNormal.y + my;
+					double tz = rotatedNormal.z + mz;
 					writer.write(String.format("  facet normal %f %f %f\n", tx, ty, tz));
 					writer.write("    outer loop\n");
-					for (Point v : List.of(t.p1, t.p2, t.p3)) {
+	
+					for (Point v : List.of(t.p3, t.p2, t.p1)) {
 						double x = v.x * sx;
 						double y = v.y * sy;
 						double z = v.z * sz;
-						x += mx;
-						y += my;
-						z += mz;
-						writer.write(String.format("      vertex %f %f %f\n", x, y, z));
+	
+						Point transformedVertex = (rotationAngle != 0) ? rotatePoint(new Point(x, y, z), rx, ry, rz, rotationAngle) : new Point(x, y, z);
+	
+						transformedVertex.x += mx;
+						transformedVertex.y += my;
+						transformedVertex.z += mz;
+	
+						writer.write(String.format("      vertex %f %f %f\n", transformedVertex.x, transformedVertex.y, transformedVertex.z));
 					}
+	
 					writer.write("    endloop\n  endfacet\n");
 				}
 				writer.write(String.format("endsolid %s\n", i));
