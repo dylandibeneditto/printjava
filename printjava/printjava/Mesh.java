2e6776a9fcd280dfc0dc8e66beb591e28ba4368d
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-03-20 10:13:11 -0400

    fix: spelling mistake

--- printjava/printjava/Mesh.java
+++ printjava/printjava/Mesh.java
@@ -7,9 +7,9 @@ public class Mesh {
 	public Point position = new Point(0,0,0);
 	// the anchor point of the mesh, the shift of the center point of a mesh
 	// (0,0,0) is the center of the mesh
-	// meshes will rotate and scale around the anchor point, and the posit describes where this anchor point is in 3d space
+	// meshes will rotate and scale around the anchor point, and the position describes where this anchor point is in 3d space
 	public Point anchor = new Point(0,0,0);
-	// the rotation of the mesh in radians
+	// the rotation of the mesh in radians or degrees, depending on the mode set in the stl file
 	public Point rotation = new Point(0,0,0);
 	// the scale of the mesh
 	// 2 corresponds to twice the size, 0.5 corresponds to half the size
