8c2f5ad9e48851e40c7c12e9d24ae2e8b57d8958
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-04-09 23:42:52 -0400

    new Quad object, 3d rectangle that can be inputted to mesh and is converted to triangles

---
+++ printjava/printjava/Quad.java
@@ -0,0 +1,27 @@
+package printjava;
+
+import printjava.Triangle;
+
+public class Quad {
+    // 4 points on the quad, will use these to construct 2 triangles
+    public Point p1;
+    public Point p2;
+    public Point p3;
+    public Point p4;
+
+    public Quad(Point p1, Point p2, Point p3, Point p4) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+    }
+
+    /**
+     * This converts the quad to 2 triangles, so that the mesh can take a quad in the 'add' function
+     */
+    public Triangle[] toTriangle() {
+        Triangle t1 = new Triangle(p1, p2, p3);
+        Triangle t2 = new Triangle(p1, p3, p4);
+        return new Triangle[] { t1, t2 };
+    }
+}
