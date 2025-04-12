88c6e6b98c18f0104b77737d0e98f4caf8141897
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-04-12 00:10:55 -0400

    more helper functions for point classes

--- printjava/printjava/Point.java
+++ printjava/printjava/Point.java
@@ -27,4 +27,97 @@ public class Point {
 		double dz = z - p.z;
 		return Math.sqrt(dx * dx + dy * dy + dz * dz);
 	}
+
+	public Point add(Point p) {
+		return new Point(this.x + p.x, this.y + p.y, this.z + p.z);
+	}
+
+	public Point subtract(Point p) {
+		return new Point(this.x - p.x, this.y - p.y, this.z - p.z);
+	}
+
+	public Point multiply(Point p) {
+		return new Point(this.x * p.x, this.y * p.y, this.z * p.z);
+	}
+
+	public Point divide(Point p) {
+		return new Point(this.x / p.x, this.y / p.y, this.z / p.z);
+	}
+
+	public double dot(Point p) {
+		return this.x * p.x + this.y * p.y + this.z * p.z;
+	}
+
+	public Point cross(Point other) {
+		double cx = this.y * other.z - this.z * other.y;
+		double cy = this.z * other.x - this.x * other.z;
+		double cz = this.x * other.y - this.y * other.x;
+		return new Point(cx, cy, cz);
+	}
+
+	/**
+	 * will rotate the point around the X AXIS
+	 * in radians!
+	 * @returns the rotated point, DOESNT CHANGE THIS POINT
+	 */
+    public Point rotateAroundX(double angleRadians, Point center) {
+        double dy = this.y - center.y;
+        double dz = this.z - center.z;
+
+        double cosA = Math.cos(angleRadians);
+        double sinA = Math.sin(angleRadians);
+
+        double newY = dy * cosA - dz * sinA;
+        double newZ = dy * sinA + dz * cosA;
+
+        return new Point(this.x, newY + center.y, newZ + center.z);
+    }
+
+	/**
+	 * will rotate the point around the Y AXIS
+	 * in radians!
+	 * @returns the rotated point, DOESNT CHANGE THIS POINT
+	 */
+    public Point rotateAroundY(double angleRadians, Point center) {
+        double dx = this.x - center.x;
+        double dz = this.z - center.z;
+
+        double cosA = Math.cos(angleRadians);
+        double sinA = Math.sin(angleRadians);
+
+        double newX = dx * cosA + dz * sinA;
+        double newZ = -dx * sinA + dz * cosA;
+
+        return new Point(newX + center.x, this.y, newZ + center.z);
+    }
+
+	/**
+	 * will rotate the point around the Z AXIS
+	 * in radians!
+	 * @returns the rotated point, DOESNT CHANGE THIS POINT
+	 */
+    public Point rotateAroundZ(double angleRadians, Point center) {
+        double dx = this.x - center.x;
+        double dy = this.y - center.y;
+
+        double cosA = Math.cos(angleRadians);
+        double sinA = Math.sin(angleRadians);
+
+        double newX = dx * cosA - dy * sinA;
+        double newY = dx * sinA + dy * cosA;
+
+        return new Point(newX + center.x, newY + center.y, this.z);
+    }
+
+	/**
+	 * will rotate the point around the X, Y, and Z AXIS
+	 * @param anglesRadians the angles in radians to rotate around the x, y, and z axes
+	 * @returns the rotated point, DOESNT CHANGE THIS POINT
+	 */
+	public Point rotate(Point anglesRadians, Point center) {
+		return new Point(this.x, this.y, this.z)
+				.rotateAroundX(anglesRadians.x, center)
+				.rotateAroundY(anglesRadians.y, center)
+				.rotateAroundZ(anglesRadians.z, center);
+	}
 }
--- printjava/printjava/Point2.java
+++ printjava/printjava/Point2.java
@@ -13,4 +13,55 @@ public class Point2 {
         this.x = x;
         this.y = y;
     }
+
+    public double distance(Point2 p) {
+        return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
+    }
+
+    public Point2 add(Point2 p) {
+        return new Point2(this.x + p.x, this.y + p.y);
+    }
+
+    public Point2 subtract(Point2 p) {
+        return new Point2(this.x - p.x, this.y - p.y);
+    }
+
+    public Point2 multiply(Point2 p) {
+        return new Point2(this.x * p.x, this.y * p.y);
+    }
+
+    public Point2 divide(Point2 p) {
+        return new Point2(this.x / p.x, this.y / p.y);
+    }
+
+    public double dot(Point2 p) {
+        return this.x * p.x + this.y * p.y;
+    }
+
+    public double cross(Point2 p) {
+        return this.x * p.y - this.y * p.x;
+    }
+
+    public Point2 rotate(double angleRadians, Point2 center) {
+        double translatedX = this.x - center.x;
+        double translatedY = this.y - center.y;
+
+        double cosA = Math.cos(angleRadians);
+        double sinA = Math.sin(angleRadians);
+
+        double rotatedX = translatedX * cosA - translatedY * sinA;
+        double rotatedY = translatedX * sinA + translatedY * cosA;
+
+        return new Point2(rotatedX + center.x, rotatedY + center.y);
+    }
+
+    public boolean equals(Point2 p) {
+        return this.x == p.x && this.y == p.y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+
 }
